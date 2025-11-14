63ea0dce6390926f77837bb995e6ea711acb3666
EdwardEmanuelIII <106100517+EdwardEmanuelIII@users.noreply.github.com>
2025-11-13 23:35:21 -0600

    Add CSRF trusted origins for webadminii website

--- RankThat/settings.py
+++ RankThat/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = 'django-insecure-b=%2rqa$zvdq^0ar!+oij_i_33am05y&*20n8s%t730b72!dc#
 DEBUG = True
 
 ALLOWED_HOSTS = ['webadminii-website.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['webadminii-website.onrender.com']
 
 
 # Application definition
