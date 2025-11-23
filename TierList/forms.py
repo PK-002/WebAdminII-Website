b8019cc030c2cb875e555e671d9ac7f3335cb4c5
EdwardEmanuelIII <emanuel.edward3@gmail.com>
2025-11-23 15:03:09 -0600

    Most of the code works

--- TierList/forms.py
+++ TierList/forms.py
@@ -1,5 +1,12 @@
 from django import forms
 
+from TierList.models import Image
+
 class TierListForm(forms.Form):
     title = forms.CharField(max_length=100)
     ranking_data = forms.JSONField()
+
+class ImageUploadForm(forms.Form):
+    class Meta:
+        model = Image
+        fields = ['ranking', 'image']
--- TierList/urls.py
+++ TierList/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('discover/', views.discover, name='discover'),
     path('maker/', views.maker, name='maker'),
-    path('maker/', views.maker, name='maker'),
+    path('new/', views.new, name='new'),
+    path('config/', views.config, name='config'),
     path('detail/<int:ranking_id>/', views.detail, name='detail'),
+    path('create/<int:ranking_id>/', views.create, name='create'),
 ]
